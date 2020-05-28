7584bd322f7d40f1021f17e3a085aee4e1b27841
amboxer21 <amboxer21@gmail.com>
2020-05-28 11:19:58 -0400

    Added some command line options.

--- src/photostream.py
+++ src/photostream.py
@@ -10,10 +10,10 @@ from optparse import OptionParser
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
-class MetaPhotostreamSelenium(type):
+class MetaPhotostream(type):
 
     def __new__(meta,name,bases,dct):
-        return super(MetaPhotostreamSelenium, meta).__new__(meta, name, bases, dct)
+        return super(MetaPhotostream, meta).__new__(meta, name, bases, dct)
 
     def __init__(cls,name,bases,dct):
         if not hasattr(cls,'count'):
@@ -28,22 +28,25 @@ class MetaPhotostreamSelenium(type):
             profile.set_preference("general.useragent.override", user_agent)
             cls.driver = webdriver.Firefox(profile)
 
-class PhotostreamSelenium(metaclass=MetaPhotostreamSelenium):
+class Photostream(metaclass=MetaPhotostream):
 
     def __init__(self,config_dict={}):
         self.email        = config_dict['email']
         self.timeout      = config_dict['timeout']
-        self.message      = config_dict['message']
-        self.dry_run      = config_dict['dry_run']
         self.password     = config_dict['password']
-        self.group_name   = config_dict['group_name']
-        self.send_message = config_dict['send_message']
+        self.username     = config_dict['username']
+        self.directory    = config_dict['directory']
+
+        if not all([self.directory, self.username]):
+            print('FB username and save directory must be specified!')
+            Photostream.driver.close()
+            sys.exit(0)
 
     def login(self):
-        PhotostreamSelenium.driver.get("https://www.facebook.com/login")
-        uname = PhotostreamSelenium.driver.find_element_by_name("email")
+        Photostream.driver.get("https://www.facebook.com/login")
+        uname = Photostream.driver.find_element_by_name("email")
         uname.send_keys(self.email)
-        pword = PhotostreamSelenium.driver.find_element_by_name("pass")
+        pword = Photostream.driver.find_element_by_name("pass")
         pword.send_keys(self.password,Keys.RETURN)
         time.sleep(5)
 
@@ -55,7 +58,7 @@ class PhotostreamSelenium(metaclass=MetaPhotostreamSelenium):
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(3)
                 try:
-                    end = self.driver.find_element_by_xpath("//*[contains(text(), 'dgdfahdfhD')]")
+                    end = self.driver.find_element_by_xpath("//*[contains(text(), 'This bogus string will never be found')]")
                     if end.text is not None:
                         break
                 except NoSuchElementException:
@@ -65,18 +68,16 @@ class PhotostreamSelenium(metaclass=MetaPhotostreamSelenium):
             pass
 
     def main(self,count=0):
-        PhotostreamSelenium.driver.get("https://www.facebook.com/codecaine21/photos_all")
+        Photostream.driver.get("https://www.facebook.com/"+str(self.username)+"/photos_all")
         self.scroll_to_bottom()
-        images = PhotostreamSelenium.driver.find_elements_by_xpath('//i[@style]')
+        images = Photostream.driver.find_elements_by_xpath('//i[@style]')
         for image in images:
             count += 1
             url = image.get_attribute("style")
             results = re.search('(background-image: url\(")(.*)("\);)', str(url), re.M | re.I)
             if results is not None:
                 try:
-                    wget.download(results.group(2),out='/home/anthony/Documents/Python/PhotoStream/Photos/')
-                    #with open('/home/anthony/Documents/Python/PhotoStream/urls.txt','a') as f:
-                        #f.write(str(results.group(2))+"\n")
+                    wget.download(results.group(2),out=self.directory)
                 except:
                     pass
 
@@ -85,21 +86,29 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-e', '--email',
         dest='email', default='example@email.com',
-        help='This is your MeetUp E-mail.')
+        help='This is your Facebook E-mail.')
     parser.add_option('-p', '--password',
         dest='password', default='password',
-        help='This is your MeetUp login password.')
+        help='This is your Facebook login password.')
     parser.add_option('-t', '--timeout',
         dest='timeout', type='int', default=120,
-        help='Blah')
+        help='How long the program will scroll to reach the bottom of your photo page.')
+    parser.add_option('-O', '--directory',
+        dest='directory',
+        help='Where the photos will be saved.')
+    parser.add_option('-u', '--username',
+        dest='username',
+        help='This is your Facebook user name.')
     (options, args) = parser.parse_args()
 
     config_dict = {
       'email'        : options.email,
       'timeout'      : options.timeout,
       'password'     : options.password,
+      'username'     : options.username,
+      'directory'    : options.directory,
     }
 
-    meetupInactivityKicker = PhotostreamSelenium(config_dict)
-    meetupInactivityKicker.login()
-    meetupInactivityKicker.main()
+    photostream = Photostream(config_dict)
+    photostream.login()
+    photostream.main()
